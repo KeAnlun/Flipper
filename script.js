7f4ea742368f1da910a2eb6d8fb4a5f77541a60e
Aaron <aaronk3@uci.edu>
2017-03-03 04:53:47 -0800

    added some styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function(){
-    var difficulty = 4;
+    var difficulty = 10;
     var linker = {};
 
     for(var i=0; i<difficulty; i++){
@@ -9,18 +9,25 @@ $(function(){
         }while(companion == i);
         linker[i] = companion;
 
-        var piece = $('<div class="piece"><button>X</button></div>');
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+        var panel = $('<button class="panel">X</button>').css('background-color', '#'+randomColor);
+        var piece = $('<div class="piece"></div>').append(panel);
         piece.attr('id', 'p-'+i);
         $('#board').append(piece);
     }
 
     $('.piece').on('click', function(event){
         console.log(event);
-        var piece = (event.currentTarget);
+        var piece = $(event.currentTarget);
         var index = piece.attr('id').substring(2);
         var companion = $('#p-' + linker[index]);
         flip(piece);
         flip(companion);
+
+        if(checkWin()){
+            window.alert('You won!');
+        }
     });
 
     function flip(piece){
@@ -29,6 +36,20 @@ $(function(){
     }
 
     function checkWin(){
-
+        var won = true;
+        var pieces = $('.piece');
+        for (var i=0; i<pieces.length; i++){
+            if($($(pieces[i]).children()[0]).text() == 'X'){
+                won = false;
+            }
+        }
+        return won;
     }
+
+    $('#reset').on('click', function(event){
+        var pieces = $('.piece');
+        for (var i=0; i<pieces.length; i++){
+            $($(pieces[i]).children()[0]).text('X')
+        }
+    })
 });
